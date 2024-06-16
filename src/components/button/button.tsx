6addead658aacbd3a2de53d76c8db95b72ffaabc
Sonny <sonnyasdev@gmail.com>
2024-06-16 11:55:48 +0300

    feat: test change button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,12 +1,22 @@
-import { PropsWithChildren } from 'react';
+import emotionStyled from '@emotion/styled';
+import { PropsWithChildren, useEffect } from 'react';
+
+const StyledButton = emotionStyled.button({
+  color: 'red',
+  backgroundColor: 'white',
+  border: '1px solid red',
+});
 
 export default function Button({
   test = 'oui',
   children,
 }: { test?: string } & PropsWithChildren) {
+  useEffect(() => {
+    console.log('oui');
+  }, []);
   return (
-    <button>
+    <StyledButton>
       {children} {test && test}
-    </button>
+    </StyledButton>
   );
 }
