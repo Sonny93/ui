6b4fa8f32e64351b877c46d6b58b180dcd505cd2
Sonny <sonnyasdev@gmail.com>
2024-06-16 11:28:57 +0300

    chore: fix vite config and remove used stuff

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,12 @@
-export default function Button() {
-  return <button>my btn</button>;
+import { PropsWithChildren } from 'react';
+
+export default function Button({
+  test = 'oui',
+  children,
+}: { test?: string } & PropsWithChildren) {
+  return (
+    <button>
+      {children} {test && test}
+    </button>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
 /// <reference types="vitest" />
 
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { dependencies } from './package.json';
+import { name, dependencies } from './package.json';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: './src/index.ts', // Specifies the entry point for building the library.
-      name: 'vite-react-ts-button', // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ['cjs', 'es'], // Specifies the output formats (CommonJS and ES modules).
+      entry: resolve(__dirname, './src/index.ts'),
+      name,
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: [...Object.keys(dependencies)], // Defines external dependencies for Rollup bundling.
+      external: [...Object.keys(dependencies)],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
-    sourcemap: true, // Generates source maps for debugging.
-    emptyOutDir: true, // Clears the output directory before building.
+    sourcemap: true,
+    emptyOutDir: true,
   },
-  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  plugins: [react(), dts({ rollupTypes: true })],
   test: {
     globals: true,
     environment: 'jsdom',
