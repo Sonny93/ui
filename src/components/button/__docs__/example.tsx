cf39904c66bec67dbc248d199179be651d2a43c4
Sonny <sonnyasdev@gmail.com>
2024-06-24 11:35:51 +0200

    feat: add css reset

--- src/components/button/__docs__/example.tsx
+++ src/components/button/__docs__/example.tsx
@@ -1,25 +1,22 @@
-import { useTheme } from '@emotion/react';
 import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import Button, { ButtonProps } from '../button';
 
-export default function Example(
+const Example = (
   props: ButtonProps &
     PropsWithChildren &
     ButtonHTMLAttributes<HTMLButtonElement>
-) {
-  const { colors } = useTheme();
-  return (
-    <div
-      style={{
-        backgroundColor: colors.background,
-        padding: '1em',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}
-    >
-      <Button {...props} />
-    </div>
-  );
-}
+) => (
+  <div
+    style={{
+      padding: '1em',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%',
+    }}
+  >
+    <Button {...props} />
+  </div>
+);
+
+export default Example;
--- src/contexts/theme_context.tsx
+++ src/contexts/theme_context.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider, css, useTheme } from '@emotion/react';
 import { PropsWithChildren, createContext, useEffect, useState } from 'react';
 import { THEME_LS_KEY } from '~/constants/theme';
 import { baseTheme } from '~/styles/themes/base_theme';
@@ -44,8 +44,135 @@ function ThemeContextProvider({
           toggleDarkTheme,
         }}
       >
+        <StyleReset />
         {children}
       </ThemeContext.Provider>
     </ThemeProvider>
+  );
+}
+
+function StyleReset() {
+  const theme = useTheme();
+  const cssReset = css({
+    '*': {
+      boxSizing: 'border-box',
+      outline: 0,
+      margin: 0,
+      padding: 0,
+      scrollBehavior: 'smooth',
+    },
+
+    '.reset': {
+      backgroundColor: 'inherit',
+      color: 'inherit',
+      padding: 0,
+      margin: 0,
+      border: 0,
+    },
+
+    a: {
+      width: 'fit-content',
+      color: theme.colors.blue.default,
+      textDecoration: 'none',
+      borderBottom: '1px solid transparent',
+    },
+
+    b: {
+      fontWeight: 600,
+      letterSpacing: '0.5px',
+    },
+
+    'h1, h2, h3, h4, h5, h6': {
+      fontWeight: '500',
+      color: theme.colors.blue.default,
+    },
+
+    kbd: {
+      textShadow: '0 1px 0 #fff',
+      fontSize: '12px',
+      color: 'rgb(51, 51, 51)',
+      backgroundColor: 'rgb(247, 247, 247)',
+      padding: '0.25em 0.5em',
+      borderRadius: '3px',
+      border: '1px solid rgb(204, 204, 204)',
+      boxShadow: '0 1px 0 rgba(0, 0, 0, 0.2), 0 0 0 2px #ffffff inset',
+      display: 'inline-block',
+    },
+
+    hr: {
+      width: '100%',
+      marginBlock: '1em',
+      border: 0,
+      borderTop: `1px solid ${theme.colors.background}`,
+    },
+  });
+
+  const documentStyle = css({
+    'html, body, #app': {
+      height: '100svh',
+      width: '100%',
+      fontFamily: "'Poppins', sans-serif",
+      fontSize: '14px',
+      color: theme.colors.font,
+      backgroundColor: theme.colors.background,
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+    },
+  });
+
+  const scrollbarStyle = css({
+    /* width */
+    '::-webkit-scrollbar': {
+      height: '0.45em',
+      width: '0.45em',
+    },
+
+    /* Track */
+    '::-webkit-scrollbar-track': {
+      borderRadius: theme.borders.radius.m,
+    },
+
+    /* Handle */
+    '::-webkit-scrollbar-thumb': {
+      background: theme.colors.blue.default,
+      borderRadius: theme.borders.radius.m,
+
+      '&:hover': {
+        background: theme.colors.blue.dark,
+      },
+    },
+  });
+
+  const tableStyle = css({
+    table: {
+      height: 'auto',
+      width: '100%',
+      borderCollapse: 'collapse',
+      borderRadius: theme.borders.radius.m,
+      overflow: 'hidden',
+    },
+
+    th: {
+      textAlign: 'center',
+      fontWeight: 400,
+      backgroundColor: theme.colors.secondary,
+    },
+
+    'td, th': {
+      padding: '0.45em',
+    },
+
+    'th, td': {
+      whiteSpace: 'nowrap',
+    },
+
+    'tr:nth-of-type(even)': {
+      backgroundColor: theme.colors.secondary,
+    },
+  });
+
+  return (
+    <Global styles={[cssReset, documentStyle, scrollbarStyle, tableStyle]} />
   );
 }
