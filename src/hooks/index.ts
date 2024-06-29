d009b37a8ec7de7e2cd2f965446c19f46ddcba2c
Sonny <sonnyasdev@gmail.com>
2024-06-29 17:23:45 +0200

    chore: export hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useClickOutside } from './use_click_outside';
+export { default as useGlobalHotkeys } from './use_global_hotkeys';
+export { default as useShortcut } from './use_shortcut';
+export { default as useThemeSwitcher } from './use_theme_switcher';
+export { default as useToggle } from './use_toggle';
